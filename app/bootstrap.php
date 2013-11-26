e4a3d04e9f189e5e8d472957e16a6a7dc37b5770
Chris Harvey <chris@chrisnharvey.com>
2013-11-26 11:15:10 +0000

    Updating bootstrap file

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,31 @@
 <?php
 
-App::before(function() {
+/*
+|--------------------------------------------------------------------------
+| Register Launching Callbacks
+|--------------------------------------------------------------------------
+|
+| Here you can register callbacks which you want to be run when your
+| application first starts. For example, you might want to instantiate
+| your main controller and open your main window.
+|
+*/
+
+App::launching(function() {
     (new MainController)->start();
+});
+
+/*
+|--------------------------------------------------------------------------
+| Register Quitting Callbacks
+|--------------------------------------------------------------------------
+|
+| Here you can register callbacks which you want to be run when your
+| application is about to quit. For example, you might want to end
+| any open connections to a third-party service.
+|
+*/
+
+App::quitting(function() {
+    
 });
