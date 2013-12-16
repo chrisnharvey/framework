f44c606855ffdeb0cec8d6ae45d85fb27c902eab
Chris Harvey <chris@chrisnharvey.com>
2013-12-16 19:45:03 +0000

    Updating providers and aliases

--- app/config/app.php
+++ app/config/app.php
@@ -5,11 +5,13 @@ return array(
     'vendor' => 'EncorePHP',
 
     'providers' => array(
-        'Encore\View\ServiceProvider'
+        'Encore\View\ServiceProvider',
+        'Encore\Console\ServiceProvider'
     ),
 
     'aliases' => array(
         'App' => 'Encore\Foundation\Facade\App',
-        'View' => 'Encore\Foundation\Facade\View'
+        'View' => 'Encore\View\Facade',
+        'Console' => 'Encore\Console\Facade',
     )
 );
