e29efcbe78f2187678f00e50dba99adae7d570f5
Chris Harvey <chris@chrisnharvey.com>
2013-12-30 13:59:04 +0000

    Adding filesystem service provider and facade

--- app/config/app.php
+++ app/config/app.php
@@ -8,12 +8,14 @@ return array(
         'Encore\View\ServiceProvider',
         'Encore\Foundation\Provider\Collection',
         'Encore\Foundation\Provider\Application',
+        'Encore\Foundation\Provider\Filesystem',
         'Encore\Console\ServiceProvider',
         'Encore\Testing\ServiceProvider'
     ),
 
     'aliases' => array(
         'App' => 'Encore\Foundation\Facade\App',
+        'Filesystem' => 'Encore\Foundation\Facade\Filesystem',
         'View' => 'Encore\View\Facade',
         'Console' => 'Encore\Console\Facade',
     )
