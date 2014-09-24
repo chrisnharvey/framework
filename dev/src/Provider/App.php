461a1f91b7190cb167a00264b2bd952fc13e5b8b
Chris Harvey <chris@chrisnharvey.com>
2014-09-24 13:36:33 +0100

    Registering commands in dev app service provider

--- dev/src/Provider/App.php
+++ dev/src/Provider/App.php
@@ -8,4 +8,22 @@ class App extends \Encore\Container\ServiceProvider
     {
         // Register dev services here
     }
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Register Commands
+    |--------------------------------------------------------------------------
+    |
+    | Here you can register commands which you can run on the console.
+    | Commands can be used to help you out with all sorts of things
+    | including compiling, debugging and more.
+    |
+    */
+
+    public function commands()
+    {
+        return [
+            'Dev\Console\Hi'
+        ];
+    }
 }
