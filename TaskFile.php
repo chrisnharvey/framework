1db63c3d9682d8a4983702b3ae149d648bd0d94c
Chris Harvey <chris@chrisnharvey.com>
2015-03-07 14:54:16 +0000

    Adding TaskFile and autoloading

---
+++ TaskFile.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see http://robo.li/
+ */
+class TaskFile extends \Encore\Development\Tasks
+{
+    // define public methods as commands
+    public function test()
+    {
+        
+    }
+}
